8d23733c7ca344fbb2bd6aed6c719e7dfb8c7bb2
rawanrumayh <62055041+rawanrumayh@users.noreply.github.com>
2020-03-22 16:23:19 +0300

    Add files via upload

---
+++ Customer.java
@@ -0,0 +1,80 @@
+public class Customer {
+
+int id;
+private String name;
+private Long phone;
+
+
+
+public Customer( int , String , String, Long){
+
+}
+
+public String toString(){
+
+}
+
+//setter and getter
+
+}//end customer class
+
+
+
+class Driver {
+
+private int id;
+private String name;
+public Driver(int id, String name){
+}
+
+
+public String toString(){
+}
+
+}//end class driver 
+
+
+class CarRental {
+
+protected numOfCars;
+
+
+public CarRental( int size){ 
+
+}
+
+public void addCar(Car c ){
+
+}
+
+
+public saveToFile() {
+
+}
+
+
+public loadFromFile(){
+
+}
+
+
+public void rentCar( String plateNo,Customer c, int numOfDay){
+
+}
+
+public void  returnCar( String plateNo) {
+
+}
+
+ 
+public VIP[] searchAvailableVIP(){
+
+}
+public Economy[] searchAvailableEconomy() {
+
+}
+public  Car getCar(PN: String){
+
+}
+
+}// end CarRental class 
