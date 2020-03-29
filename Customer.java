99df5da9f81989faed5569da34623f0b37a5867e
rawanrumayh <62055041+rawanrumayh@users.noreply.github.com>
2020-03-29 22:33:25 +0300

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -10,30 +10,25 @@ private Long phone;
 
 
 public Customer( int id , String  name , Long  phone){
-this .id=id;this.name=name;this.phone=phone;
-}
+this .id=id;this.name=name;this.phone=phone;}
 
 public String toString(){
-return "the customer id >>"+id + "the customer name >> \t"+ name +" the customer phone >> \t"+phone;
-}
-
+return "the customer id >>"+id + "the customer name >> \t"+ name +" the customer phone >> \t"+phone;}
 
 
 public int getId(){
 
-return id;
+return id;}
 
-}
 
 public String getName(){
-return name;
+return name;}
 
-}
 
-public String getPhone(){
-return phone;
+public Long getPhone(){
+return phone;}
 
-}
+
 //setter and getter
 
 }//end customer class
@@ -48,14 +43,12 @@ private String name;
 
 public Driver(int id, String name){
 this.id=id;
-this.name=name;
-}
+this.name=name;}
 
 
 public String toString(){
 
- return "the Driver id is >> "+id  +"the Driver name is >> " +name;
-}
+ return "the Driver id is >> "+id  +"the Driver name is >> " +name;}
 
 }//end class driver 
 
@@ -63,10 +56,12 @@ public String toString(){
 class CarRental {
 
 private  int numOfCars ;
+private Car [] carlist;
 
 
 public  CarRental( int size){ //VIP or Economy
 numOfCars=0;
+carlist = new Car [size];
 
 //Array 
 }
@@ -76,7 +71,7 @@ if(numOfCars<carlist.length){
 carlist[numOfCars++]= c;
 System.out.println("Added successfully ");}
 else
-Syastem.out.println("adding failed");
+System.out.println("adding failed");
 
 }
 
@@ -91,23 +86,123 @@ public  void loadFromFile(){
 }
 
 
-public void rentCar( String plateNo,Customer c, int numOfDay){
+public void rentCar( String plateNo,Customer c, int numOfDay){ // not sure
+for (int i=0; i<numOfCars; i++)
+if ( carlist[i].getPlateNo().equals(plateNo) ){
+carlist[i].setAvailability(false);
+carlist[i].setCustomer(c);
+System.out.println(carlist[i].printBill(numOfDay));}
 
-}
+}// end rent car
 
 public void  returnCar( String plateNo) {
-
+for (int i=0; i<numOfCars; i++)
+if ( carlist[i].getPlateNo().equals(plateNo) ) 
+carlist[i].setAvailability(true);
 }
 
+
+//
  
 public VIP[] searchAvailableVIP(){
+VIP [] AvailableVIP = new VIP [numOfCars];
+int count =0;
+for (int i=0; i<numOfCars; i++)
+if ( carlist[i] instanceof VIP ) {
+if ( carlist[i].getAvailability() )
+AvailableVIP [count++] = (VIP)(carlist[i]); }
 
-}
+return AvailableVIP;} //end searchvip
+
+//
+
 public Economy[] searchAvailableEconomy() {
+Economy [] AvailableEco = new Economy [numOfCars];
+int count =0;
 
-}
+for (int i=0; i<numOfCars; i++)
+if ( carlist[i] instanceof Economy ) {
+if ( carlist[i].getAvailability() )
+AvailableEco [count++] = (Economy)(carlist[i]); }
+
+return AvailableEco;} // end searcheco
+
+
+//
+
 public  Car getCar( String PN){
-  //.equals(PN)
-  }
+for (int i=0; i<numOfCars; i++)
+if ( carlist[i].getPlateNo().equals(PN) )
+return carlist[i];
+ 
+return null;  }
 
 }// end CarRental class 
+
+
+abstract class Car implements Payabel //carlist in all sub classes and here + relation between customer class
+{
+
+protected String plateNo;
+protected double pricePerDay;
+protected String model;
+protected String color;
+protected boolean available;
+protected Customer customer;
+//  *object coustmer in car class
+
+public Car ( String plate, double rice, String model, String color) {
+plateNo = plate; pricePerDay = rice; this.model = model; this.color = color; 
+} //end const
+
+public void setCustomer(Customer c ){
+customer = c;
+} //end setcustomer
+
+public String toString(){ 
+return "Plate number: "+plateNo+"\t price per day: "+pricePerDay+"\t model: "+model+"\t color: "+color+"\n"; }
+
+public boolean getAvailability () { return available;}
+
+public void setAvailability (boolean t) { available = t;}
+
+public String getPlateNo() { return plateNo; }
+
+
+//setters getters
+}//end class car
+
+
+class Economy extends Car {
+
+public Economy ( String plate, double price, String model, String color){
+super ( plate, price, model, color);}//end cons
+
+
+public String printBill(int days){
+double price =0;
+price = pricePerDay * days;
+
+if (days>7)
+price = price - (price*0.2);
+
+return "Economy bill: /n the price for "+days+" days is: "+price;}//end printbill
+}//end economy class
+
+
+class VIP extends Car {
+
+Driver d;//object
+
+public VIP ( String plate, double price, String model, String color,Driver d){
+super ( plate, price, model, color); this.d=d; }//end cons
+//driver
+
+public String printBill(int days){
+double price =0;
+price = pricePerDay * days+(10*days);
+
+return "VIP bill: /n the price for "+days+" days is: "+price;}//end printbill
+}//end VIP class
+
+
