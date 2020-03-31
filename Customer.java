79a68062fac0ccc61de8704fa99982c19f4a208d
Saraalqhtani8 <Saraalqhtani1419@gmail.com>
2020-03-31 20:19:25 +0300

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -240,6 +240,22 @@ price =super. pricePerDay * days+(10*days);
 String s="VIP bill: \n"+super.toString()+"\n";
 s=s+"Driver :"+d.toString()+"\n";
 s=s+"the price:"+price+"\n";
+String FileName=getCustomer.getName()+"_"+getCustomer.getId()+".text";
+try{
+
+//open file text
+
+File OUTF=new File (fileName);
+FileOutputStream SF=new FileOutputStream(OUTF);
+PrintWriter PW=new PrintWriter(SF);
+PW.println(s);
+PW.close();}
+
+catch(IOException E){
+JOptionPane.showMessageDialog(null,"Error ViP file ");}//text
+
+return s;
+
 }//end printbill
 }//end VIP class
 
